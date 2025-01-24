85f000a475ad9a1be9c7a353e93d3ba38fd60edc
AngelRots <meowsrail@gmail.com>
2025-01-24 13:24:11 +0100

    Changed WAV Data order

--- src/audiolib.h
+++ src/audiolib.h
@@ -1,9 +1,7 @@
 #include <stdint.h>
 
-
 #ifndef AUDIOLIB_H
 #define AUDIOLIB_H
-
 
 typedef unsigned char BYTE;
 
@@ -11,25 +9,23 @@ typedef unsigned char BYTE;
 #define WAV_LINEARPCM   1
 #define WAV_STEREO      2
 
-//#pragma pack(push, 1) // Ensure no padding is added
-struct WAVHeader_s // WAV Structure [44 Bytes]
+#pragma pack(push, 1)   // Ensure no padding is added
+struct WAVHeader_s      // WAV Structure [44 Bytes]
 {
-    char RIFF[4]; // Identifier « RIFF »
-    char FTYPE[4]; // file type ("WAVE")
-    char FMT[4]; // ("fmt ")
-    char DATACHUNK[4]; // "data" chunk ID
-    int32_t SIZE; // size of file - RIFF identifier
-    int32_t LEN; // Length of format data
-    int16_t AFORMAT; // Audio format
-    int32_t SMPLRATE; // Sample rate
-    int32_t BPERSEC; // Byte rate
-    int16_t CHANNELS; // Number of channels
-    int16_t BITDEPTH; // Bits per sample
-    int32_t DATASZ; // Size of data chunk
-}; 
-//#pragma pack(pop)
-
-
-
+    char RIFF[4];       // Identifier "RIFF"
+    int32_t SIZE;       // size of file - 8 bytes
+    char FTYPE[4];      // file type ("WAVE")
+    char FMT[4];        // "fmt " sub-chunk
+    int32_t LEN;        // Length of format data
+    int16_t AFORMAT;    // Audio format
+    int16_t CHANNELS;   // Number of channels
+    int32_t SMPLRATE;   // Sample rate
+    int32_t BPERSEC;    // Byte rate
+    int16_t BLOCKALIGN; // Block align
+    int16_t BITDEPTH;   // Bits per sample
+    char DATACHUNK[4];  // "data" sub-chunk
+    int32_t DATASZ;     // Size of data chunk
+};
+#pragma pack(pop)
 
 #endif /* audiolib_h */
