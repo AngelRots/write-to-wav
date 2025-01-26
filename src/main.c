c61f53713814bff06a66e8a193a9f0d44f33e744
AngelRots <meowsrail@gmail.com>
2025-01-26 13:14:54 +0100

    Reworked Main

--- src/main.c
+++ src/main.c
@@ -3,9 +3,10 @@
 #include <string.h>
 #include <math.h>
 
-#include "audiolib.h"
+#include "fmt_wav.h"
 #include "audiodef.h"
 #include "os_dep.h"
+#include "xclock.h"
 
 #define SDL_MAIN_HANDLED // Avoid Main() method before sdl.h
 #include <SDL3/SDL.h>
@@ -13,7 +14,6 @@
 
 #define PI      3.14159265358979323846
 #define MAX_AMP 32767
-
 
 void gui_placeholder()
 {
@@ -85,19 +85,8 @@ int main(void)
 
     memcpy(wav.DATACHUNK, "data", 4);
 
-    wav.LEN = 16;                       // PCM
-    wav.AFORMAT = WAV_LINEARPCM;        // PCM
-    wav.SMPLRATE = CD_SMPLRATE;         // Sample rate
-    wav.CHANNELS = WAV_STEREO;          // Stereo
-    wav.BITDEPTH = 16;                  // Bits per sample
-    wav.BPERSEC = wav.SMPLRATE * wav.CHANNELS * (wav.BITDEPTH / 8);     // Bytes per second
-    wav.BLOCKALIGN = wav.CHANNELS * (wav.BITDEPTH / 8);                 // Block align
+    InitRIFF(&wav, WAV_LINEARPCM, CD_SMPLRATE, WAV_STEREO, 16);
 
-    int numSamples = CD_SMPLRATE;                                       // number of samples for 1 second of audio
-    wav.DATASZ = numSamples * wav.CHANNELS * (wav.BITDEPTH / 8);        // Size of audio data
-
-  
-    wav.SIZE = wav.DATASZ - 8; // RIFF Chunk Size
 
     printf("Enter a filename: ");
     scanf("%s", filename);
@@ -111,7 +100,7 @@ int main(void)
     }
 
     printf("Checking WAV Header Size...\n");
-    slp(1);
+    xsleep(1);
     if (sizeof(wav) != WAV_EXSIZE)
     {
         printf("WAV Header size doesn't match! (%zu)\n",sizeof(wav));
@@ -133,7 +122,7 @@ int main(void)
     
 
     SBYTE* audioData = (SBYTE*)calloc(wav.DATASZ, sizeof(SBYTE));
-    for (int i = 0; i < numSamples; i++)
+    for (int i = 0; i < wav.SMPLRATE; i++)
     {
         int sampleValue = MAX_AMP * cos((2 * PI * NOTE_C3 * i) / wav.SMPLRATE);
 
@@ -141,7 +130,7 @@ int main(void)
         audioData[i * 2 + 1] = (SBYTE)((sampleValue >> 8) & 0xFF);   // Upper byte
     }
     printf("Writing Raw Data...\n");
-    slp(1);
+    xsleep(1);
     fwrite(audioData, sizeof(SBYTE), wav.DATASZ, fp);
     printf("Data Written!\n");
     printf("WAV Size: %.1fKB\n", round(wav.DATASZ / 1024.0));
