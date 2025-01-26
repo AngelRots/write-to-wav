d28116903dea9563fd9f69e8419149fd32f60928
AngelRots <meowsrail@gmail.com>
2025-01-26 18:29:00 +0100

    The great purge

--- src/vgui/vgui_base.c
+++ src/vgui/vgui_base.c
@@ -2,63 +2,30 @@
 #include "vgui_base.h"
 
 
-/**
- * @brief          Creates a window using SDL3 with a structure's parameters.
- * 
- * @param win      The pointer to the SDL window. 
- * @param rend     The pointer to the SDL renderer.
- * @param vgui     The address of the VGUI structure.
- */
-void vgui_base(SDL_Window*win,SDL_Renderer*rend,struct vgui* vgui)
+
+
+
+void vgui_button(SDL_Renderer*rend,struct VGUIButton* vbutton)
 {
-  
-
-    if(SDL_Init(SDL_INIT_VIDEO || SDL_INIT_EVENTS) != 1) /* Returns true on success, false otherwise. */
-    {
-        printf("SDL wasn't initialized correctly!\n");
-        SDL_Log("SDL Error: %s",SDL_GetError());
-        exit(-3);
-    }
-
-    win = SDL_CreateWindow(vgui->handle,vgui->w,vgui->h,0);
-    if(win == NULL)
-    {
-        SDL_Log("SDL Window Error: %s",SDL_GetError());
-        exit(-4);
-    }
-
-    rend = SDL_CreateRenderer(win,NULL);
     if(rend == NULL)
     {
         SDL_Log("SDL Renderer Error: %s",SDL_GetError());
         exit(-5);
-    }
-    
-    SDL_Event LIB_EVENT;
-    
-    int noInit = 0;
-    while(!noInit)
-    {
-        while(SDL_PollEvent(&LIB_EVENT)) 
-        {
-            switch(LIB_EVENT.type) 
-            {
-                case SDL_EVENT_QUIT:
-                SDL_Log("SDL3 Event Exception!");
-                noInit = 1;
-                break;
-            }
-        }
-        SDL_SetRenderDrawColor(rend,vgui->r,vgui->g,vgui->b,0xff);
-        SDL_RenderClear(rend);
-        SDL_RenderPresent(rend);
-        SDL_Delay(1);
+    } 
 
-        
-    }
-    SDL_Log("SDL Shutdown!");
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+
+   SDL_FRect quickref;
+   quickref.x = vbutton->b_x;
+   quickref.y = vbutton->b_y;
+   quickref.w = vbutton->b_w;
+   quickref.h = vbutton->b_h;
+
+
+   SDL_SetRenderDrawColor(rend,vbutton->b_r,vbutton->b_g,vbutton->b_b,SDL_ALPHA_OPAQUE);
+
+   SDL_RenderFillRect(rend,&quickref);
+   
+   SDL_RenderPresent(rend);
+   SDL_Delay(1);
 
 }
--- src/vgui/vgui_base.h
+++ src/vgui/vgui_base.h
@@ -3,17 +3,34 @@
 
 #include <SDL3/SDL.h>
 
-struct vgui 
+struct VGUI 
 {
     const char* handle; // Window Name
     int w;              // Window Height
     int h;              // Window Width
-    Uint8 r;           
-    Uint8 g;
-    Uint8 b;
+    Uint8 r;            // Red
+    Uint8 g;            // Green
+    Uint8 b;            // Blue
+    
+    // TODO: Implement Inheritance
+    
+   struct VGUIButton  
+    {
+        const char* BID;  // Button ID 
+        int b_w;          // Button Width
+        int b_h;          // Button Height
+        int b_x;          // Button X Coord 
+        int b_y;          // Button Y Coord
+        Uint8 b_r;        // Red
+        Uint8 b_g;        // Green
+        Uint8 b_b;        // Blue
+
+    };
+ 
 };
 
-void vgui_base(SDL_Window*win,SDL_Renderer*rend,struct vgui* vgui);
+
+void vgui_button(SDL_Renderer*rend,struct VGUIButton*vbutton);
 
 
 #endif /* vgui_base_h */
