6418fc0172e73d81761cb8285583d3dcd1d31671
AngelRots <meowsrail@gmail.com>
2025-01-26 18:28:15 +0100

    Moved Rendering to Main

--- src/main.c
+++ src/main.c
@@ -16,19 +16,101 @@
 
 int main(void)
 {
-    struct vgui vgui;
+    struct VGUI vgui;
+    struct VGUIButton vbutton;
     SDL_Window* winbase = NULL;
     SDL_Renderer* rendbase = NULL;
 
     
     vgui.handle = "Meow!";
-    vgui.h = 800;
-    vgui.w = 600;
+    vgui.w = 800;
+    vgui.h = 600;
     vgui.r = 0;
     vgui.g = 0;
     vgui.b = 150;
 
-    vgui_base(winbase,rendbase,&vgui);
+    vbutton.b_w = 64;
+    vbutton.b_h = 64;
+    vbutton.b_x = 100;
+    vbutton.b_y = 100;
+    
+    vbutton.b_r = 255;
+    vbutton.b_g = 255;
+    vbutton.b_b = 255;
+
+
+    
+
+
+
+    if(SDL_Init(SDL_INIT_VIDEO || SDL_INIT_EVENTS) != 1) /* Returns true on success, false otherwise. */
+    {
+        printf("SDL wasn't initialized correctly!\n");
+        SDL_Log("SDL Error: %s",SDL_GetError());
+        exit(-3);
+        
+    }
+
+    winbase = SDL_CreateWindow(vgui.handle,vgui.w,vgui.h,0);
+    if(winbase == NULL)
+    {
+        SDL_Log("SDL Window Error: %s",SDL_GetError());
+        exit(-4);
+    }
+
+    rendbase = SDL_CreateRenderer(winbase,NULL);
+    if(rendbase == NULL)
+    {
+        SDL_Log("SDL Renderer Error: %s",SDL_GetError());
+        exit(-5);
+    }
+    
+    SDL_Event LIB_EVENT;
+    
+    int noInit = 0;
+    while(!noInit)
+    {
+        while(SDL_PollEvent(&LIB_EVENT)) 
+        {
+            switch(LIB_EVENT.type) 
+            {
+                case SDL_EVENT_QUIT:
+                SDL_Log("SDL3 Event Exception!");
+                noInit = 1;
+                break;
+            }
+            if(SDL_EVENT_MOUSE_MOTION == LIB_EVENT.type)
+            {
+                float mouse_x, mouse_y; 
+
+                SDL_GetMouseState(&mouse_x,&mouse_y);
+                printf("Mouse X:%f\n",mouse_x);
+
+            }
+        }
+        SDL_SetRenderDrawColor(rendbase, vgui.r, vgui.g, vgui.b, 0xff);
+        SDL_RenderClear(rendbase);
+          vgui_button(rendbase,&vbutton);     
+        SDL_RenderPresent(rendbase);
+        SDL_Delay(1);
+
+        
+    }
+    SDL_Log("SDL Shutdown!");
+    SDL_DestroyRenderer(rendbase);
+    SDL_DestroyWindow(winbase);
+    SDL_Quit();
+
+
+
+
+
+
+
+
+    
+
+   
     
 
     struct WAVHeader_s wav;
